61187ee03f53fd266fdc78b01399300ddcdd8c7d
rishabmps <rishab.mps@gmail.com>
2016-11-13 04:50:36 +0530

    Added facebook Service to get image

--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,40 @@
 var express = require('express');
 var router = express.Router();
-var facebookService = require('../service/facebook')('1617228834969795','eaa57dfdbb3a243c83d3b1a17762cc40');
+var facebookService = require('../service/facebook')('1617228834969795', 'eaa57dfdbb3a243c83d3b1a17762cc40');
+var User = require('../models/userModel');
 
-
-router.use('/',function(req,res,next){
-	if(!req.user){
-		res.redirect('/');
-	}
-	next();
-})
-/* GET users listing. */
+router.use('/', function(req, res, next) {
+        if (!req.user) {
+            res.redirect('/');
+        }
+        next();
+    })
+    /* GET users listing. */
 router.get('/', function(req, res) {
-  if(req.user.facebook){
-	facebook.getImage(req.user.facebook.token,
-		function  (results) {
-			req.users.facebook.image = results.url;
-		res.render('users',{user:req.user})
-		})
-  }else{
+    if (!req.user.image) {
+        facebookService.getImageAndEmail(req.user.facebook.token,
+            function(results) {
 
-  }
-  res.render('users',{user: {name: req.user.displayName,
-  							image: req.user.image}});
+                var query = {
+                    'facebook.id': req.user.facebook.id
+                };
+                User.findOne(query, function(error, user) {
+                    user.image = results.picture.data.url;
+                    user.email = results.email;
+                    user.save();
+                });
+                res.render('users', {
+                    user: req.user
+                });
+
+            }
+        );
+    } else {
+        res.render('users', {
+            user: req.user
+        });
+    }
+
 });
 
 module.exports = router;
--- service/facebook.js
+++ service/facebook.js
@@ -1,27 +1,33 @@
 var OAuth = require('OAuth').OAuth2;
+
 var facebook = function  (facebookKey,facebookSecret) {
 	var key = facebookKey;
 	var secret = facebookSecret;
 
 	var oauth = new OAuth(
-		key, secret, 'https://graph.facebook.com'
+		key, secret, 'https://graph.facebook.com',
 		null,
-		'oauth/token'
+		'oauth2/token',
 		null
-		);
+	);
 
-	var getImage = function  (userKey, done) {
-		oauth.get('https://graph.facebook.com/v2.3/me/picture?
-			redirect=false&type=large',
-			userKey,function  (err,results,res) {
+	var getImageAndEmail = function  (userKey, done) {
+		oauth.get('https://graph.facebook.com/v2.3/me?fields=email,picture&redirect=false',
+			userKey,
+			function  (err,results,res) {
+				if (err) console.error('error: ' + err);
+				console.log(results);
 				results = JSON.parse(results);
-				done(results.data);
+
+				done(results);
 			}
 		);
 	}
 
+
 	return{
-		getImage: getImage;
+		getImageAndEmail: getImageAndEmail,
+
 	}
 }
 
